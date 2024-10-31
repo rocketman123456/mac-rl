90c1dc7000ed20438df96f9fbcbb48cd489d047a
Zhang Yufeng <759094438@qq.com>
2024-10-31 21:40:30 +0800

    [ADD] add some public model for testing [ADD] add first code for mujoco simulation

---
+++ simple_mujoco.py
@@ -0,0 +1,29 @@
+import mujoco
+import mujoco.viewer
+import time
+import threading
+
+
+# Load the model
+model = mujoco.MjModel.from_xml_path("unitree_h1/scene.xml")
+data = mujoco.MjData(model)
+
+viewer = mujoco.viewer.launch(model, data)
+mujoco.mj_forward(model, data)
+
+
+def sync_loop():
+    while viewer.is_running():
+        viewer.sync()
+        time.sleep(0.010)
+
+
+thread = threading.Thread(target=sync_loop)
+thread.start()
+# Run a viewer to interact with the simulation
+while viewer.is_running():
+    # mujoco.mj_step(model, data)
+    mujoco.mj_forward(model, data)
+    time.sleep(0.010)
+
+thread.join()
